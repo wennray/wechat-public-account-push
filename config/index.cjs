05ea6d88f3715f0f7548767468cdf9681bda82c9
wennray <115437810+wennray@users.noreply.github.com>
2022-10-10 10:08:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9jEU6R9TcjnGdaAtLh4h_kEigMs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '_s9jP3xbGB-O7IVO9k9Rpz0RXMIdXu3vDG0WvkWejyg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
